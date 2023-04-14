58636909271ff537dc1cf72dbbe71d374a561fa1
Zacky Yaser Malik Gumiwang <zackyyaser50@gmail.com>
2023-04-14 13:11:02 +0700

    Updates

---
+++ precache-manifest.a837a8e1ec1449435ec72905ad205aeb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e10694ec45c87c92f60",
+    "url": "/Assignment-3/static/css/main.b1657407.chunk.css"
+  },
+  {
+    "revision": "5e10694ec45c87c92f60",
+    "url": "/Assignment-3/static/js/main.5e10694e.chunk.js"
+  },
+  {
+    "revision": "eeb70bfc1150a369d55d",
+    "url": "/Assignment-3/static/css/1.bf55d3fb.chunk.css"
+  },
+  {
+    "revision": "eeb70bfc1150a369d55d",
+    "url": "/Assignment-3/static/js/1.eeb70bfc.chunk.js"
+  },
+  {
+    "revision": "b8d911eef6a241cc22f7",
+    "url": "/Assignment-3/static/js/runtime~main.b8d911ee.js"
+  },
+  {
+    "revision": "3cab70b4299e77696bd4ba92b7d081c2",
+    "url": "/Assignment-3/static/media/COVID19.3cab70b4.jpg"
+  },
+  {
+    "revision": "08db8d3b6fca11579ce244ff0c62cf45",
+    "url": "/Assignment-3/index.html"
+  }
+];
